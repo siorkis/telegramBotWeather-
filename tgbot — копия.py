3bb18908df7ae36b0ec87de36272951ea7e6df58
siorkis <siorkis@mail.ru>
2020-04-18 05:59:58 -0700

    Update tgbot — копия.py

--- tgbot — копия.py
+++ tgbot — копия.py
@@ -1,27 +1,27 @@
-import pyowm
-import telebot
+import pyowm              # weather's library 
+import telebot            # bot's library 
 
 
-tb = telebot.TeleBot("TOKEN")
+tb = telebot.TeleBot("TOKEN") # bot's identification 
 
 
-@tb.message_handler(commands=['start'])
+@tb.message_handler(commands=['start']) # bot sends the welcome message 
 def send_welcome(message):
     tb.send_message(message.chat.id, "Привет, в каком вы городе/стране?\n")
 
 
-@tb.message_handler(content_types=["text"])
+@tb.message_handler(content_types=["text"]) # bot reads the massege from user
 def send_message(message):
-    try:
-        owm = pyowm.OWM('e70a43b8a010c59d5309454867f6e529', language="ru")
-        observation = owm.weather_at_place(message.text)
-    except:
-        tb.send_message(message.chat.id, "Хмм... я такого города и тем более страны не знаю ((")
+    try:                                     # checking user's message, does it city/country or not
+        owm = pyowm.OWM('API', language="ru") 
+        observation = owm.weather_at_place(message.text) #taking city from data base 
+    except:                                              # case of wrong input 
+        tb.send_message(message.chat.id, "Хмм... я такого города и тем более страны не знаю ((") 
         tb.send_message(message.chat.id, "Попробуй еще раз"
                                          ".")
-    else:
-        w = observation.get_weather()
-        temp = w.get_temperature('celsius')['temp']
+    else:                                                 # case of correct input
+        w = observation.get_weather()                     # getting information about weather in indicated place
+        temp = w.get_temperature('celsius')['temp']       # getting information about temperature in indicated place
         answer = message.text + " - место, где сейчас " + w.get_detailed_status() + ".\n"
         answer += "Текущая температура: " + str(temp) + ' градусов цельсия\n\n'
         if temp <= 10.0:
@@ -30,8 +30,8 @@ def send_message(message):
             answer += "На улице не так холодно, но легкую куртку лучше надеть!\n"
         else:
             answer += "На улице достаточно тепло, одевайся как хочешь)\n"
-        tb.send_message(message.chat.id, answer)
-        tb.send_message(message.chat.id, "Для какого города хотите еще узнать погоду?")
+        tb.send_message(message.chat.id, answer)           # bot sends message with weather to user 
+        tb.send_message(message.chat.id, "Для какого города хотите еще узнать погоду?") # bot sends the ending massege
 
 
-tb.polling(none_stop=True)
+tb.polling(none_stop=True)       # bot don't stop sending messages 
